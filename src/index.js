56133091ede6eb175a545b9fdc11d0abcd42ccea
Karina Kupriyanovich <kupriyanovichk72@gmail.com>
2018-03-23 17:21:24 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Construction from './components/Construction/Construction';
 import MainPage from './components/MainPage/MainPage';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
 	    <Route exact path={process.env.PUBLIC_URL + '/'} component={Construction} />
       <Route path={process.env.PUBLIC_URL + '/cv'} component={MainPage} />
     </Switch>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('main')
 );
