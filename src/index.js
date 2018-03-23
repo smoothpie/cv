0bab1fbfd274cfc193c2362d44c3af1bd4ed2062
Karina Kupriyanovich <kupriyanovichk72@gmail.com>
2018-03-23 17:37:54 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import MainPage from './components/MainPage/MainPage';
 ReactDOM.render(
   <HashRouter>
     <Switch>
-	    <Route exact path={process.env.PUBLIC_URL + '/'} component={Construction} />
+	    <Route path={process.env.PUBLIC_URL + '/'} component={Construction} />
       <Route path={process.env.PUBLIC_URL + '/cv'} component={MainPage} />
     </Switch>
   </HashRouter>,
