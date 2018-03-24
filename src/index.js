d7165d3769524b86c31b05238367cf4422bad4aa
smoothpie <kupriyanovichk72@gmail.com>
2018-03-24 01:49:08 -0400

    Added homepage

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Construction from './components/Construction/Construction';
 import MainPage from './components/MainPage/MainPage';
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <Switch>
-	    <Route exact path={process.env.PUBLIC_URL + '/'} component={Construction} />
-      <Route path={process.env.PUBLIC_URL + '/cv'} component={MainPage} />
+	    <Route path={process.env.PUBLIC_URL + '/'} component={Construction} />
+      <Route path='/cv' component={MainPage} />
     </Switch>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('main')
 );
