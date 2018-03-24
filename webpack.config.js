c4ed9153b5bff62a786232a56e7c644bf38d96b3
smoothpie <kupriyanovichk72@gmail.com>
2018-03-24 00:25:54 -0400

    Fixed path for images

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   entry: './src/index.js',
   context: path.resolve(__dirname),
   output: {
-    publicPath: '/',
+    publicPath: '',
     path: path.resolve(__dirname, 'docs'),
     filename: `index.js`,
   },
